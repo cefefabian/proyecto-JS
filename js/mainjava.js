ee28be03abd4000654391f6ee0de04b8b3d44213
Cefe fabian <cefefabian6@gmail.com>
2022-06-13 13:58:51 -0300

    funcion y if

--- js/mainjava.js
+++ js/mainjava.js
@@ -1,15 +1,27 @@
-let resultado = 750;
-let cuantos = parseInt(prompt('Elige cuantos chocolates queres'))
-    
 
-if (cuantos <= 10){
-    for (let i = 750; cuantos > 0 ; i++) {
-        let resultado = i *cuantos;
+  
+function nombre() {
+    let nombreIngresado = prompt ('ingrese su nombre')
+    alert('Hola ' + nombreIngresado + '. Bienvenido a milkaShop!')
+}
+nombre()
+
+
+let cuantos = parseInt(prompt('Elige cuantos chocolates queres cada uno tiene un valor de 750 pesos'))
+let precio = 750;
+if ((cuantos <= 10) && (cuantos > 0)){
+    for (let i = 1; i <= 10; i++){
+        let resultado = cuantos * precio;
         alert('El precio a pagar es de ' + resultado + ' pesos')
         break;
     }
 }
 
-else {
+else if (cuantos > 10)  {
     alert('El maximo son 10')
 }
+
+else {
+    alert('Por favor ingrese cuantos chocolates desea')
+}
+
