21c83473cdf009dbafa44a665b03eff080410072
Cefe fabian <cefefabian6@gmail.com>
2022-07-05 13:45:25 -0300

    carrito pt2

--- js/mainjava.js
+++ js/mainjava.js
@@ -1,14 +1,59 @@
+let usuario
 
-let usuario = prompt ('Ingrese su nombre')
+let usuarioEnLS = localStorage.getItem('usuario')
 
-nombres = []
-
-nombres.push(usuario);
-for (const nombre of nombres) {
-    let saludo = document.querySelector(".caja h1")
-    saludo.innerHTML = `${nombre} bienvenido a milkaShop`;
+if (usuarioEnLS != null) {
+  usuario = usuarioEnLS
+} else {
+  usuario = prompt('ingrese')
+  localStorage.setItem('usuario', usuario)
 
 }
+let saludo = document.querySelector(".caja h1")
+saludo.innerHTML = `${usuario} bienvenido a milkaShop`;
+
+
+let logoCarrito = document.querySelector('.logo')
+logoCarrito.innerHTML = `<p class="contador">0</p>
+`
+
+const claseLogoCarrito = document.querySelector('.contador').textContent
+let btnCarrito = document.querySelector('#productos');
+let contador = document.querySelector(".contador")
+let sumar = 1;
+
+document.querySelector(".contador").innerText;
+if(claseLogoCarrito === '0'){
+  logoCarrito.hidden = true
+  contador.hidden = true
+}
+btnCarrito.addEventListener("click", function (e) {
+  if(e.target.className === "comprar"){
+    logoCarrito.hidden = false
+  contador.hidden = false
+	contador.innerText = parseInt(contador.innerText) + sumar;
+  }
+  
+});
+
+let tarjetaTitulo = document.querySelector('.tarjeta-titulo').textContent
+
+let main = document.querySelector('main')
+
+btnCarrito.addEventListener('click', (e) =>{
+  if (e.target.className === "comprar") {
+  const alert = document.querySelector('#alert')
+  alert.innerHTML = `<div class= "alert">
+                        <h3>Chocolate ${tarjetaTitulo} añadido al carrito</h3>
+                      </div>`
+                               
+  setTimeout(function () {
+    alert.classList.add("visible");
+  }, 2000);
+  alert.classList.remove("visible");
+  }
+})
+
 
 
 const botonComprar = document.querySelectorAll('.comprar')
@@ -28,6 +73,12 @@ botonComprar.forEach((comprar) => {
         const tarjetaPrecio = tarjeta.querySelector('.precio').textContent;
         const tarjetaImg = tarjeta.querySelector('.tarjeta-img').src;
 
+        htmlCarrito(tarjetaTitulo, tarjetaPrecio, tarjetaImg)
+    }
+
+
+      function htmlCarrito(tarjetaTitulo, tarjetaPrecio, tarjetaImg) {
+        
         const chocolateNuevo = document.createElement('div');
         chocolateNuevo.classList.add ('carrito-compra')
         const contenidoTajeta = ` <div class="carrito-img">
@@ -40,13 +91,38 @@ botonComprar.forEach((comprar) => {
                                    <h4>${tarjetaPrecio}</h6>
                                  </div>
                                  <div class="carrito-cancelar">
-                                   <input type="number" value="1">
+                                   <input class="cantidad" type="number" value="1">
                                   <button>
                                      X
                                    </button>
                                  </div>`;
  
          chocolateNuevo.innerHTML = contenidoTajeta;
-         carrito.append(chocolateNuevo);
-    }
+         carrito.append(chocolateNuevo); 
+
+         totalDelCarrito()
+      }
+
+      function totalDelCarrito() {
+        let total = 0
+        const precioTotalDelCarrito = document.querySelector('.preciototal')
+
+        const tarjeta = document.querySelectorAll('.carrito-compra')
+
+        tarjeta.forEach((tarjeta) => {
+          const precioDelChocolate = tarjeta.querySelector('.carrito-precio')
+          const soloPrecio = Number(precioDelChocolate.textContent.replace('$', ''));
+
+          const cantidad = tarjeta.querySelector('.cantidad')
+          const cantidad2 = Number(cantidad.value);
+
+          total = total + soloPrecio * cantidad2
+        });
+      
+        precioTotalDelCarrito.innerHTML = `Total ${total}$`;
+      }
+      
+      
+    
+    
 
