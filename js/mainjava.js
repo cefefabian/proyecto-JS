6e1a0e7d69bbc934031091274ad4f77dd0473a6f
Cefe fabian <cefefabian6@gmail.com>
2022-07-25 10:45:32 -0300

    proyecto terminado

--- js/mainjava.js
+++ js/mainjava.js
@@ -44,10 +44,11 @@ fetch('chocolates.json')
     })
 
 class Chocolates{
-  constructor(tipo, precio, img){
+  constructor(tipo, precio, img, cantidad){
     this.tipo = tipo,
     this.precio = precio,
-    this.img = img
+    this.img = img,
+    this.cantidad = 1
   }
 
 }
@@ -58,41 +59,54 @@ const botonComprar = document.querySelectorAll('.comprar')
 
 const carrito = document.querySelector('.carrito')
 
+const inputElemnto = carrito.getElementsByClassName("cantidad");
+
+
+
 
 botonComprar.forEach((comprar) => {
     comprar.addEventListener('click', alCarrito);
+  })
 
     function alCarrito(evento) {
       const boton = evento.target;
       const tarjeta = boton.closest('.tarjeta')
+      
+      const tarjetaTitulo = tarjeta.querySelector('.tarjeta-titulo').textContent;
+      const tarjetaPrecio = tarjeta.querySelector('.precio').textContent;
+      const tarjetaImg = tarjeta.querySelector('.tarjeta-img').src;
+      
+      for (let i = 0; i < arrChocolates.length; i++) {
+        if (arrChocolates[i].tipo.trim() === tarjetaTitulo.trim()){
+          inputElemnto[i].value++          
+          totalDelCarrito()
+          return;
+        }}
 
-        const tarjetaTitulo = tarjeta.querySelector('.tarjeta-titulo').textContent;
-        const tarjetaPrecio = tarjeta.querySelector('.precio').textContent;
-        const tarjetaImg = tarjeta.querySelector('.tarjeta-img').src;
-
-        const Chocolate = new Chocolates(tarjetaTitulo, tarjetaPrecio, tarjetaImg)
-        arrChocolates.push(Chocolate)
+      const Chocolate = new Chocolates(tarjetaTitulo, tarjetaPrecio, tarjetaImg)
+      arrChocolates.push(Chocolate)
         generarHtml(arrChocolates)    
         
-        
-    }   
-  })
+      }   
 
-
-      function generarHtml(arr) {    
- const chocolateNuevo = document.createElement('div');
-        chocolateNuevo.classList.add ('carrito-compra')
+   
+    function generarHtml(arr) {    
+      const chocolateNuevo = document.createElement('div');
+      chocolateNuevo.classList.add ('carrito-compra')
+      const contenerdorCarritoPadre = document.querySelector('.contenedor-carrito-padre')
+      
+      contenerdorCarritoPadre.append(chocolateNuevo)
         arr.map((el) => chocolateNuevo.innerHTML = ` <div class="carrito-img">
                                     <img src=${el.img} alt="">
                                  </div>
                                 <div class="carrito-titulo">
-                                  <h4>${el.tipo}</h6>
+                                  <h4>${el.tipo}</h4>
                                  </div>
                                  <div class="carrito-precio">
-                                   <h4>${el.precio}</h6>
+                                   <h4>${el.precio}</h4>
                                  </div>
                                  <div class="carrito-cancelar">
-                                   <input class="cantidad" type="number" value="1">
+                                   <input class="cantidad" min="1" type="number" value=${el.cantidad}>
                                   <button class="eliminar">
                                      X
                                    </button>
@@ -111,15 +125,10 @@ botonComprar.forEach((comprar) => {
          .addEventListener('change', quantityChanged)
         
          totalDelCarrito()
-
-         const btnn = document.querySelector('.compra-final-input')
-         if (arrChocolates.length != 0) {
-           btnn.removeAttribute('disabled')
-           btnn.classList.add('btnn')
-          }
-       
+     
+        botonDesactivado()
       }
-      
+
     
 
       const container = document.getElementsByClassName('carrito-compra')
@@ -151,24 +160,48 @@ botonComprar.forEach((comprar) => {
   
       }
 
-
-
-    function eliminarChocolate(evento) {
-      const botonEliminar = evento.target;
-      botonEliminar.closest('.carrito-compra').remove()
-      Toastify({
-        text: "Chocolate eliminado del carrito correctamente",
-        duration: 2500,
-        className: 'alert'
-        }).showToast();
-      totalDelCarrito()
-    }
+      function eliminarChocolate(evento) {
+        const botonEliminar = evento.target;
+        botonEliminar.closest('.carrito-compra').remove()
+        arrChocolates.length = 0;
+        
+        Toastify({
+          text: "Chocolate eliminado del carrito correctamente",
+          duration: 2500,
+          className: 'alert'
+          }).showToast();
+        totalDelCarrito()
+      }
 
     function quantityChanged(evento) {
       const input = evento.target;
       input.value <= 0 ? (input.value = 1) : null
       totalDelCarrito()
     }
+
+    function botonDesactivado() {
+      const btnn = document.querySelector('.compra-final-input')
+      if (arrChocolates.length != 0) {
+        btnn.removeAttribute('disabled')
+        btnn.classList.add('btnn')
+      }
+    }
+ window.addEventListener('scroll', ()=>{
+  window.onscroll = ()=> {
+    let scroll =document.documentElement.scrollTop
+    if (scroll > 100) {
+      document.getElementById('saludo').style.top = '-120px';
+      
+    }else{
+      document.getElementById('saludo').style.top = '0';
+    }
+    
+  };
+ })
+
+
+
+
 
 console.log(arrChocolates);
 
--- js/productos.js
+++ js/productos.js
@@ -3,6 +3,39 @@ let price = document.querySelector('.price')
 let precioEnLS = localStorage.getItem('precio')
 precioEnLS = price.innerHTML = `Total a pagar ${precioEnLS}$`
 
+const btnComprar = document.querySelector('.btn-comprar')
+
+let inputNombre = document.querySelector('.input-nombre')
+let inputTarjeta = document.querySelector('.input-tarjeta')
+let inputEmail = document.querySelector('.input-email')
+let inputCvu = document.querySelector('.input-cvu')
+let inputs = document.querySelectorAll('.input')
+inputs.forEach((finalizarCompra) =>{
+
+  finalizarCompra.addEventListener('keydown', ()=>{
+      btnComprar.removeAttribute('disabled')
+  })
+})
+
+
+btnComprar.addEventListener('click', ()=>{
+    let nombre = inputNombre.value;
+    Swal.fire({
+        icon: "success",
+        text: "Gracias por su compra" + ' ' + nombre
+    })
+    inputEmail.value = ''
+    inputTarjeta.value = ''
+    inputCvu.value = ''
+    inputNombre.value = ''
+    btnComprar.setAttribute('disabled', true)
+   
+})
+
+
+
+
+
 
     
 
